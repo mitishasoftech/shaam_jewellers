223d93a3b71744b26a07ddd015b35698ca62f922
Mona Lisa <shivam43chd@gmail.com>
2024-06-21 18:40:17 +0530

    added images

--- src/productfolder/Products.js
+++ src/productfolder/Products.js
@@ -1,5 +1,29 @@
 import React from 'react'
 import './Products.css'
+import Women1 from '../picsofproducts/women1.jpg'
+import Women2 from '../picsofproducts/women2.jpg'
+import Women3 from '../picsofproducts/women3.jpg'
+import Women4 from '../picsofproducts/women4.jpg'
+import Women5 from '../picsofproducts/women5.jpg'
+import Women6 from '../picsofproducts/women6.jpg'
+import Women7 from '../picsofproducts/women7.jpg'
+import Women8 from '../picsofproducts/women8.jpg'
+import Women9 from '../picsofproducts/women9.jpg'
+import Women10 from '../picsofproducts/women10.jpg'
+import Men1 from '../picsofproducts/men1.jpg'
+import Men2 from '../picsofproducts/men2.jpg'
+import Men3 from '../picsofproducts/men3.jpg'
+import Men4 from '../picsofproducts/men4.jpg'
+import Men5 from '../picsofproducts/men5.jpg'
+import Men6 from '../picsofproducts/men6.jpg'
+import Men7 from '../picsofproducts/men7.jpg'
+import Men8 from '../picsofproducts/men8.jpg'
+import Men9 from '../picsofproducts/men9.jpg'
+import Men10 from '../picsofproducts/men10.jpg'
+
+
+
+
 
 const Products = () => {
   return (
@@ -8,102 +32,102 @@ const Products = () => {
       <h1 style={{textAlign:'center', fontWeight:'bold' , marginTop:50}}>PRODUCTS</h1>
       <div className='productsContainer'>
         <div className='productsContent'>
-            <img src='https://shamjewellers.com/wp-content/uploads/2017/06/2.jpg'></img>
+            <img src={Women1}></img>
             <p>RIWAAZ</p>
             <p>Royal Union</p>
         </div>
         <div className='productsContent'>
-            <img src='https://shamjewellers.com/wp-content/uploads/2017/06/1.jpg'></img>
+            <img src={Women2}></img>
             <p>RIWAAZ</p>
             <p>Royal Union</p>
         </div>
         <div className='productsContent'>
-            <img src='https://shamjewellers.com/wp-content/uploads/2017/06/3.jpg'></img>
+            <img src={Women3}></img>
             <p>RIWAAZ</p>
             <p>Royal Union</p>
         </div>
         <div className='productsContent'>
-            <img src='https://shamjewellers.com/wp-content/uploads/2017/06/4.jpg'></img>
+            <img src={Women4}></img>
             <p>RIWAAZ</p>
             <p>Royal Union</p>
         </div>
         <div className='productsContent'>
-            <img src='https://shamjewellers.com/wp-content/uploads/2018/06/Website-Sham-05.jpg'></img>
+            <img src={Women5}></img>
             <p>RIWAAZ</p>
             <p>Royal Union</p>
         </div>
         <div className='productsContent'>
-            <img src='https://shamjewellers.com/wp-content/uploads/2018/06/Website-Sham-06.jpg'></img>
+            <img src={Women6}></img>
             <p>RIWAAZ</p>
             <p>Royal Union</p>
         </div>
         <div className='productsContent'>
-            <img src='https://shamjewellers.com/wp-content/uploads/2017/06/2.jpg'></img>
+            <img src={Women7}></img>
             <p>RIWAAZ</p>
             <p>Royal Union</p>
         </div>
         <div className='productsContent'>
-            <img src='https://shamjewellers.com/wp-content/uploads/2017/06/2.jpg'></img>
+            <img src={Women8}></img>
             <p>RIWAAZ</p>
             <p>Royal Union</p>
         </div>
         <div className='productsContent'>
-            <img src='https://shamjewellers.com/wp-content/uploads/2017/06/2.jpg'></img>
+            <img src={Women9}></img>
             <p>RIWAAZ</p>
             <p>Royal Union</p>
         </div>
         <div className='productsContent'>
-            <img src='https://shamjewellers.com/wp-content/uploads/2017/06/2.jpg'></img>
+            <img src={Women10}></img>
             <p>RIWAAZ</p>
             <p>Royal Union</p>
         </div>
         <div className='productsContent'>
-            <img src='https://shamjewellers.com/wp-content/uploads/2017/06/2.jpg'></img>
+            <img src={Men1}></img>
             <p>RIWAAZ</p>
             <p>Royal Union</p>
         </div>
         <div className='productsContent'>
-            <img src='https://shamjewellers.com/wp-content/uploads/2017/06/2.jpg'></img>
+            <img src={Men2}></img>
             <p>RIWAAZ</p>
             <p>Royal Union</p>
         </div>
         <div className='productsContent'>
-            <img src='https://shamjewellers.com/wp-content/uploads/2017/06/2.jpg'></img>
+            <img src={Men3}></img>
             <p>RIWAAZ</p>
             <p>Royal Union</p>
         </div>
         <div className='productsContent'>
-            <img src='https://shamjewellers.com/wp-content/uploads/2017/06/2.jpg'></img>
+            <img src={Men4}></img>
             <p>RIWAAZ</p>
             <p>Royal Union</p>
         </div>
         <div className='productsContent'>
-            <img src='https://shamjewellers.com/wp-content/uploads/2017/06/2.jpg'></img>
+            <img src={Men5}></img>
             <p>RIWAAZ</p>
             <p>Royal Union</p>
         </div>
         <div className='productsContent'>
-            <img src='https://shamjewellers.com/wp-content/uploads/2017/06/2.jpg'></img>
+            <img src={Men6}></img>
             <p>RIWAAZ</p>
             <p>Royal Union</p>
         </div>
         <div className='productsContent'>
-            <img src='https://shamjewellers.com/wp-content/uploads/2017/06/2.jpg'></img>
+            <img src={Men7}></img>
             <p>RIWAAZ</p>
             <p>Royal Union</p>
         </div>
         <div className='productsContent'>
-            <img src='https://shamjewellers.com/wp-content/uploads/2017/06/2.jpg'></img>
+            <img src={Men8}></img>
             <p>RIWAAZ</p>
             <p>Royal Union</p>
         </div>
         <div className='productsContent'>
-            <img src='https://shamjewellers.com/wp-content/uploads/2017/06/2.jpg'></img>
+            <img src={Men9}></img>
             <p>RIWAAZ</p>
             <p>Royal Union</p>
         </div>
         <div className='productsContent'>
-            <img src='https://shamjewellers.com/wp-content/uploads/2017/06/2.jpg'></img>
+            <img src={Men10}></img>
             <p>RIWAAZ</p>
             <p>Royal Union</p>
         </div>
